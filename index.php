8210cd053038b0a79169023f70cd97ead497a358
David Taylor <under.my.control@gmail.com>
2017-06-24 04:33:35 -0500

    adding env dump

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 
-echo "<h1>Welcome to the best PHP application in the world.</h1><h3>yeahh booooooy</h3>";
+echo "<h1>Welcome to the best PHP application in the world.</h1><h3>yeahh booooooy</h3><pre>";
+
 var_dump($_SERVER);
+var_dump($_ENV);
+echo "</pre>";
