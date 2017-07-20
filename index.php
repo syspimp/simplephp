e06a4d4c65b8f055a443b7d991ba9596c8a5075d
David Marsel Taylor <dataylor@forty.maskedadmins.com>
2017-07-20 10:15:24 -0400

    ...

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
-echo "<h1>Welcome to the best PHP application in the world.</h1><h3>yeahh booooooy</h3><pre>";
-
+echo "<h1>Welcome to the end of the demonstration.</h1><h3>Success/h3>";
+echo "<p>Play around</p>";
+echo "<p>access the <a href='https://ose-cluster-master.stack.maskedadmins.com:8443/console/'>Openshift Container Platform console</a></p>";
+echo "<p>access the <a href='https://tower.stack.maskedadmins.com'>Redhat Openstack 10</a></p>";
+echo "<p>access the <a href='https://tower.stack.maskedadmins.com'>Ansible Tower console</a></p>";
+echo "<p>access the <a href='https://cloudforms.stack.maskedadmins.com'>Cloudforms console</a></p>";
+echo "<p>access the <a href='https://satellite.stack.maskedadmins.com'>Satellite console</a></p>";
+echo "<p>Variable Dumps</p><pre>";
 var_dump($_SERVER);
 var_dump($_ENV);
 echo "</pre>";
