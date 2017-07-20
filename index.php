1a24b29c0a9191dbf9e36acb6af46f692d9d2b2e
David Marsel Taylor <dataylor@forty.maskedadmins.com>
2017-07-20 12:37:08 -0400

    ...

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 echo "<h1>Welcome to the end of the demonstration.</h1><h3>Success/h3>";
 echo "<p>Play around</p>";
 echo "<p>access the <a href='https://ose-cluster-master.stack.maskedadmins.com:8443/console/'>Openshift Container Platform console</a></p>";
-echo "<p>access the <a href='https://tower.stack.maskedadmins.com'>Redhat Openstack 10</a></p>";
-echo "<p>access the <a href='https://tower.stack.maskedadmins.com'>Ansible Tower console</a></p>";
-echo "<p>access the <a href='https://cloudforms.stack.maskedadmins.com'>Cloudforms console</a></p>";
-echo "<p>access the <a href='https://satellite.stack.maskedadmins.com'>Satellite console</a></p>";
+echo "<p>access the <a href='https://workshopper-redhat-lab.ose.maskedadmins.com:8444'>OCP workshops</a></p>";
+echo "<p>access the <a href='https://compute4.maskedadmins.com:8445'>Redhat Openstack 10</a></p>";
+echo "<p>access the <a href='https://tower.stack.maskedadmins.com:8446'>Ansible Tower console</a></p>";
+echo "<p>access the <a href='https://cloudforms.stack.maskedadmins.com:8447'>Cloudforms console</a></p>";
+echo "<p>access the <a href='https://satellite.stack.maskedadmins.com:8448'>Satellite console</a></p>";
+echo "<p>access the <a href='https://rzp12552.live.dynatrace.com'>Dynatrace Monitoring</a></p>";
 echo "<p>Variable Dumps</p><pre>";
 var_dump($_SERVER);
 var_dump($_ENV);
